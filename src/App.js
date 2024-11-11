216a3f91113d4afeb18b8588ce7b17af445ed182
sevde <nurtaskin2@gmail.com>
2024-11-11 23:19:36 +0300

    Added Cart

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ProductList from "./components/ProductList";
 import ProductDetails from "./components/ProductDetails";
 import Navbar from "./components/Navbar";
+import Cart from "./components/Cart";
 import "./App.css";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
+  function removeFromCart(productId) {
+    setCart(cart.filter(item => item.id !== productId));
+  }
+
   return (
     <Router>
       <div className="App">
         <Navbar />
         <Routes>
-          <Route path="/" element={<ProductList />} />
-          <Route path="/product/:id" element={<ProductDetails />} />
+          {/* Ana sayfa rotası, tüm ürünlerin listelendiği sayfa */}
+          <Route path="/" element={<ProductList addToCart={addToCart} />} />
+
+          {/* Ürün detay sayfası rotası, seçilen ürünün detayları gösterilir */}
+          <Route path="/products/:id" element={<ProductDetails addToCart={addToCart} />} />
         </Routes>
+
+        {/* Sepet bileşenini tüm sayfalarda göstermek için */}
+        <Cart cart={cart} removeFromCart={removeFromCart} />
+
       </div>
     </Router>
   );
