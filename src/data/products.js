1b952555bfece30eb55fcd094f2b6bc53e0c8f7a
sevde <nurtaskin2@gmail.com>
2024-11-11 23:18:59 +0300

    Added  image support to product data

--- src/data/products.js
+++ src/data/products.js
@@ -1,6 +1,6 @@
 const products = [
-    { id: 1, name: 'Laptop', price: 1200, description: 'A high performance laptop' },
-    { id: 2, name: 'Smartphone', price: 800, description: 'A smartphone with a great camera' },
-    { id: 3, name: 'Tablet', price: 500, description: 'A tablet for reading books' },
+    { id: 1, name: 'Laptop', price: 1200, description: 'A high performance laptop', image: "photos/laptop-foto.jpg" },
+    { id: 2, name: 'Smartphone', price: 800, description: 'A smartphone with a great camera', image: "photos/telefon-foto.png" },
+    { id: 3, name: 'Tablet', price: 500, description: 'A tablet for reading books', image: "photos/tablet-foto.jpg" },
 ];  
  export default products;
